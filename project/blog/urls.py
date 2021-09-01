2321c7d3d8915ebd5c8a4350d6b7e7f3733b4e56
taba <tabatabaeipour.sa@gmail.com>
2021-09-01 18:47:55 +0430

    add post detail template and url

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path( ''             , views.index        , name= 'index'       ),
-    path( 'posts'        , views.posts        , name='posts'        ),
-    path( 'post/<slug>'  , views.post_detail  ,  name='post-detail' )
+    path( ''                  , views.index        , name='index'        ),
+    path( 'posts/'             , views.posts        , name='posts'        ),
+    path( 'post/<slug:slug>'  , views.post_detail  , name='post-detail'  )
 
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request , 'blog/index.html')
-
-def posts (requests):
     pass
 
-def post_detail (request):
-    pass
+def posts (request):
+    return render(request , 'blog/posts.html')
+
+def post_detail (request , slug):
+    return render(request , 'blog/post-detail.html')
